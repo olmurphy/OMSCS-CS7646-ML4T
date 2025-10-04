1c9f4d9d40d58d1c9afff5220e7f8f3b1a73add6
Owen Murphy <owen261@icloud.com>
2025-10-04 14:04:55 +0800

    updated

--- ML4T_2025Fall/marketsim/marketsim.py
+++ ML4T_2025Fall/marketsim/marketsim.py
@@ -22,8 +22,8 @@ GT honor code violation.
 -----do not edit anything above this line---  		  	   		 	 	 		  		  		    	 		 		   		 		  
   		  	   		 	 	 		  		  		    	 		 		   		 		  
 Student Name: Tucker Balch (replace with your name)  		  	   		 	 	 		  		  		    	 		 		   		 		  
-GT User ID: tb34 (replace with your User ID)  		  	   		 	 	 		  		  		    	 		 		   		 		  
-GT ID: 900897987 (replace with your GT ID)  		  	   		 	 	 		  		  		    	 		 		   		 		  
+GT User ID: omurphy8 (replace with your User ID)
+GT ID: 904015662
 """  		  	   		 	 	 		  		  		    	 		 		   		 		  
   		  	   		 	 	 		  		  		    	 		 		   		 		  
 import datetime as dt  		  	   		 	 	 		  		  		    	 		 		   		 		  
@@ -32,7 +32,45 @@ import os
 import numpy as np  		  	   		 	 	 		  		  		    	 		 		   		 		  
   		  	   		 	 	 		  		  		    	 		 		   		 		  
 import pandas as pd  		  	   		 	 	 		  		  		    	 		 		   		 		  
-from util import get_data, plot_data  		  	   		 	 	 		  		  		    	 		 		   		 		  
+from util import get_data, plot_data
+
+def author():  		  	   		 	 	 		  		  		    	 		 		   		 		  
+    """  		  	   		 	 	 		  		  		    	 		 		   		 		  
+    :return: The GT username of the student  		  	   		 	 	 		  		  		    	 		 		   		 		  
+    :rtype: str  		  	   		 	 	 		  		  		    	 		 		   		 		  
+    """  		  	   		 	 	 		  		  		    	 		 		   		 		  
+    return "omurphy8"	  	 
+
+def study_group():
+    """
+    Returns
+        A comma separated string of GT_Name of each member of your study group
+        # Example: "gburdell3, jdoe77, tbalch7" or "gburdell3" if a single individual working alone
+    """
+    return "omurphy8"
+
+def get_stats(port_val, daily_risk_free_rate=0.0):
+    """
+    This function computes and returnsthe portfolio statistics like cumulative return, average daily return,
+    stdev of daily return and Sharpe ratio of the given prices in the portfolio
+
+    :param allocs: list of allocations for each stock
+    :param prices: Pandas Dataframe holding data of stock prices
+    """
+
+    # daily returns minus the first return
+    daily_rets = (port_val / port_val.shift(1)) - 1
+    daily_rets = daily_rets[1:]
+
+    cum_ret = (port_val[-1] / port_val[0]) - 1
+    avg_daily_ret = daily_rets.mean()
+    std_daily_ret = daily_rets.std()
+
+    # computing annualized Sharpe ratio, assuming 252 trading days
+    # with a risk-free rate of 0%
+    sr = np.sqrt(252) * ((avg_daily_ret - daily_risk_free_rate) / std_daily_ret)
+
+    return cum_ret, avg_daily_ret, std_daily_ret, sr
   		  	   		 	 	 		  		  		    	 		 		   		 		  
   		  	   		 	 	 		  		  		    	 		 		   		 		  
 def compute_portvals(  		  	   		 	 	 		  		  		    	 		 		   		 		  
@@ -61,17 +99,59 @@ def compute_portvals(
     # TODO: Your code here  		  	   		 	 	 		  		  		    	 		 		   		 		  
   		  	   		 	 	 		  		  		    	 		 		   		 		  
     # In the template, instead of computing the value of the portfolio, we just  		  	   		 	 	 		  		  		    	 		 		   		 		  
-    # read in the value of IBM over 6 months  		  	   		 	 	 		  		  		    	 		 		   		 		  
-    start_date = dt.datetime(2008, 1, 1)  		  	   		 	 	 		  		  		    	 		 		   		 		  
-    end_date = dt.datetime(2008, 6, 1)  		  	   		 	 	 		  		  		    	 		 		   		 		  
-    portvals = get_data(["IBM"], pd.date_range(start_date, end_date))  		  	   		 	 	 		  		  		    	 		 		   		 		  
-    portvals = portvals[["IBM"]]  # remove SPY  		  	   		 	 	 		  		  		    	 		 		   		 		  
-    rv = pd.DataFrame(index=portvals.index, data=portvals.values)  		  	   		 	 	 		  		  		    	 		 		   		 		  
-  		  	   		 	 	 		  		  		    	 		 		   		 		  
-    return rv  		  	   		 	 	 		  		  		    	 		 		   		 		  
-    return portvals  		  	   		 	 	 		  		  		    	 		 		   		 		  
-  		  	   		 	 	 		  		  		    	 		 		   		 		  
-  		  	   		 	 	 		  		  		    	 		 		   		 		  
+    # read in the value of IBM over 6 months  	
+
+    # read & process orders
+    orders_df = pd.read_csv(orders_file, index_col="Date", parse_dates=True, na_values=["nan"])
+    orders_df.sort_index(inplace=True)
+
+    # get date range & symb
+    start_date = orders_df.index.min()
+    end_date = orders_df.index.max()
+    all_dates = pd.date_range(start_date, end_date)
+    symbols = orders_df["Symbol"].unique().tolist()
+    
+    if 'SPY' not in symbols:
+        symbols.append('SPY')
+
+    # get prices for all symb & date range
+    prices = get_data(symbols, pd.date_range(start_date, end_date))
+    prices = prices.drop("SPY", axis=1) # removing spy
+    prices["cash"] = 1.0  # add a cash column for cash
+
+    dates = prices.index
+
+    trades = pd.DataFrame(0, index=dates, columns=prices.columns)
+    for date, row in orders_df.iterrows():
+        symbol = row['Symbol']
+        order = row['Order']
+        shares = row['Shares']
+
+        price_at_trade = prices.loc[date, symbol]
+        trade_cost = shares * price_at_trade
+        if order == "BUY":
+            trades.loc[date, symbol] += shares
+            trades.loc[date, 'cash'] -= trade_cost + (impact * trade_cost) + commission
+        elif order == "SELL":
+            trades.loc[date, symbol] -= shares
+            trades.loc[date, 'cash'] += trade_cost - (impact * trade_cost) - commission
+    print("-----trades-----")
+    print(trades)
+    # init holdings for symb
+    holdings = trades.copy()
+    holdings.loc[holdings.index[0], 'cash'] += start_val
+    holdings = holdings.cumsum()
+    print("-----holdings-----")
+    print(holdings)
+
+    # cacl daily port val
+    portvals = (holdings * prices).sum(axis=1)
+    print(portvals)
+    return portvals
+
+
+
+
 def test_code():  		  	   		 	 	 		  		  		    	 		 		   		 		  
     """  		  	   		 	 	 		  		  		    	 		 		   		 		  
     Helper function to test code  		  	   		 	 	 		  		  		    	 		 		   		 		  
@@ -80,32 +160,27 @@ def test_code():
     # note that during autograding his function will not be called.  		  	   		 	 	 		  		  		    	 		 		   		 		  
     # Define input parameters  		  	   		 	 	 		  		  		    	 		 		   		 		  
   		  	   		 	 	 		  		  		    	 		 		   		 		  
-    of = "./orders/orders2.csv"  		  	   		 	 	 		  		  		    	 		 		   		 		  
+    of = "./orders/orders-10.csv"  		  	   		 	 	 		  		  		    	 		 		   		 		  
+    # of = "./orders/orders-02.csv"  		  	   		 	 	 		  		  		    	 		 		   		 		  
     sv = 1000000  		  	   		 	 	 		  		  		    	 		 		   		 		  
   		  	   		 	 	 		  		  		    	 		 		   		 		  
     # Process orders  		  	   		 	 	 		  		  		    	 		 		   		 		  
-    portvals = compute_portvals(orders_file=of, start_val=sv)  		  	   		 	 	 		  		  		    	 		 		   		 		  
+    portvals = compute_portvals(orders_file=of, start_val=sv)
     if isinstance(portvals, pd.DataFrame):  		  	   		 	 	 		  		  		    	 		 		   		 		  
         portvals = portvals[portvals.columns[0]]  # just get the first column  		  	   		 	 	 		  		  		    	 		 		   		 		  
     else:  		  	   		 	 	 		  		  		    	 		 		   		 		  
         "warning, code did not return a DataFrame"  		  	   		 	 	 		  		  		    	 		 		   		 		  
   		  	   		 	 	 		  		  		    	 		 		   		 		  
     # Get portfolio stats  		  	   		 	 	 		  		  		    	 		 		   		 		  
-    # Here we just fake the data. you should use your code from previous assignments.  		  	   		 	 	 		  		  		    	 		 		   		 		  
-    start_date = dt.datetime(2008, 1, 1)  		  	   		 	 	 		  		  		    	 		 		   		 		  
-    end_date = dt.datetime(2008, 6, 1)  		  	   		 	 	 		  		  		    	 		 		   		 		  
-    cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = [  		  	   		 	 	 		  		  		    	 		 		   		 		  
-        0.2,  		  	   		 	 	 		  		  		    	 		 		   		 		  
-        0.01,  		  	   		 	 	 		  		  		    	 		 		   		 		  
-        0.02,  		  	   		 	 	 		  		  		    	 		 		   		 		  
-        1.5,  		  	   		 	 	 		  		  		    	 		 		   		 		  
-    ]  		  	   		 	 	 		  		  		    	 		 		   		 		  
-    cum_ret_SPY, avg_daily_ret_SPY, std_daily_ret_SPY, sharpe_ratio_SPY = [  		  	   		 	 	 		  		  		    	 		 		   		 		  
-        0.2,  		  	   		 	 	 		  		  		    	 		 		   		 		  
-        0.01,  		  	   		 	 	 		  		  		    	 		 		   		 		  
-        0.02,  		  	   		 	 	 		  		  		    	 		 		   		 		  
-        1.5,  		  	   		 	 	 		  		  		    	 		 		   		 		  
-    ]  		  	   		 	 	 		  		  		    	 		 		   		 		  
+    # Here we just fake the data. you should use your code from previous assignments.  		  	   		 	 	 		  	
+
+    cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = get_stats(portvals)
+    start_date = portvals.index.min()
+    end_date = portvals.index.max()	    	 		 		   		
+
+    spy_prices = get_data(['SPY'], pd.date_range(start_date, end_date))
+    spy_prices = spy_prices['SPY'] 		  
+    cum_ret_SPY, avg_daily_ret_SPY, std_daily_ret_SPY, sharpe_ratio_SPY = get_stats(spy_prices)		  	   		 	 	 		  		  		    	 		 		   		 		  
   		  	   		 	 	 		  		  		    	 		 		   		 		  
     # Compare portfolio against $SPX  		  	   		 	 	 		  		  		    	 		 		   		 		  
     print(f"Date Range: {start_date} to {end_date}")  		  	   		 	 	 		  		  		    	 		 		   		 		  
@@ -126,4 +201,21 @@ def test_code():
   		  	   		 	 	 		  		  		    	 		 		   		 		  
   		  	   		 	 	 		  		  		    	 		 		   		 		  
 if __name__ == "__main__":  		  	   		 	 	 		  		  		    	 		 		   		 		  
-    test_code()  		  	   		 	 	 		  		  		    	 		 		   		 		  
+    test_code()	  	   		 	 	 		  		  		    	 		 		   		 		  
+
+# MarketsimTestCase(  		  	   		 	 	 		  		  		    	 		 		   		 		  
+#         description="Orders 10, impact and commission",  		  	   		 	 	 		  		  		    	 		 		   		 		  
+#         group="both",  		  	   		 	 	 		  		  		    	 		 		   		 		  
+#         inputs=dict(  		  	   		 	 	 		  		  		    	 		 		   		 		  
+#             orders_file="orders-10.csv",  		  	   		 	 	 		  		  		    	 		 		   		 		  
+#             start_val=1000000,  		  	   		 	 	 		  		  		    	 		 		   		 		  
+#             commission=9.95,  		  	   		 	 	 		  		  		    	 		 		   		 		  
+#             impact=0.005,  		  	   		 	 	 		  		  		    	 		 		   		 		  
+#         ),  		  	   		 	 	 		  		  		    	 		 		   		 		  
+#         outputs=dict(  		  	   		 	 	 		  		  		    	 		 		   		 		  
+#             num_days=141,  		  	   		 	 	 		  		  		    	 		 		   		 		  
+#             last_day_portval=1026658.3265,  		  	   		 	 	 		  		  		    	 		 		   		 		  
+#             sharpe_ratio=0.627643575702,  		  	   		 	 	 		  		  		    	 		 		   		 		  
+#             avg_daily_ret=0.000222013722594,  		  	   		 	 	 		  		  		    	 		 		   		 		  
+#         ),  		  	   		 	 	 		  		  		    	 		 		   		 		  
+#     ), 
